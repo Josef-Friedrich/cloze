f51e2b292da0234b69ede35202cbcd23828575ca
Josef Friedrich <josef@friedrich.rocks>
2020-07-06 19:36:47 +0200

    Remove unused test files

--- build.lua
+++
@@ -1,8 +0,0 @@
-#!/ usr/bin/env texlua
-module = "cloze"
-
-checkengines = {"lualatex"}
-stdengine = "lualatex"
-
-kpse.set_program_name ("kpsewhich")
-dofile (kpse.lookup ("l3build.lua"))
--- texlive.profile
+++
@@ -1,5 +0,0 @@
-# https://www.tug.org/texlive/doc/install-tl.html#PROFILES
-selected_scheme scheme-minimal
-TEXDIR /usr/local/texlive/
-TEXMFHOME ~/texmf
-instopt_portable 1
