3e0a4d62da4c4a8390ad82962622acea51766f46
Josef Friedrich <josef@friedrich.rocks>
2020-07-07 14:41:07 +0200

    Implement basic lpeg parser for key value options

--- cloze.lua
+++ cloze.lua
@@ -28,6 +28,95 @@ if not modules then modules = { } end modules ['cloze'] = {
   license   = 'The LaTeX Project Public License Version 1.3c 2008-05-04'
 }
 
+local lpeg = require('lpeg')
+
+--- See [lpeg.P](http://www.inf.puc-rio.br/~roberto/lpeg#op-p)
+--
+-- Like `"literal"` in peg.js.
+local Pattern = lpeg.P
+
+--- See [lpeg.R](http://www.inf.puc-rio.br/~roberto/lpeg#op-r)
+--
+-- Like `[a-z]` in peg.js.
+local Range = lpeg.R
+
+--- See [lpeg.S](http://www.inf.puc-rio.br/~roberto/lpeg#op-s)
+--
+-- Like `[characters]` in peg.js.
+local Set = lpeg.S
+
+--- See [lpeg.C](http://www.inf.puc-rio.br/~roberto/lpeg#cap-c)
+local capture = lpeg.C
+
+--- See [lpeg.Ct](http://www.inf.puc-rio.br/~roberto/lpeg#cap-t)
+local capture_table = lpeg.Ct
+
+--- See [lpeg.Cf](http://www.inf.puc-rio.br/~roberto/lpeg#cap-f)
+local capture_fold = lpeg.Cf
+
+--- See [lpeg.Cg](http://www.inf.puc-rio.br/~roberto/lpeg#cap-g)
+local capture_group = lpeg.Cg
+
+--- A naive key value parser written with Lpeg to get rid of kvoptions.
+--
+-- * `patt^0` = `expression *` (peg.js)
+-- * `patt^1` = `expression +` (peg.js)
+-- * `patt1 * patt2` = `expression1 expression2` (peg.js)
+-- * `patt1 + patt2` = `expression1 / expression2` (peg.js)
+--
+-- * [TUGboat article: Parsing complex data formats in LuaTEX with LPEG](https://tug.org/TUGboat/tb40-2/tb125menke-lpeg.pdf)
+-- * [Dimension handling](https://github.com/lualatex/lualibs/blob/master/lualibs-util-dim.lua)
+--
+-- @tparam string input The key value options in a unparsed fashion as a
+--   string.
+--
+-- @treturn table The key value options as a table.
+local function key_value_parser(input)
+  local sign = Set('-+')
+  local integer = Range('09')^1
+  local number = integer^0 * Pattern('.')^0 * integer^0
+
+  local unit =
+    Pattern('pt') +
+    Pattern('cm') +
+    Pattern('mm') +
+    Pattern('sp') +
+    Pattern('bp') +
+    Pattern('in') +
+    Pattern('pc') +
+    Pattern('dd') +
+    Pattern('cc')
+
+  local dimension = capture(sign^0 * number * unit^0)
+
+  local white_space = Set(' \t\r\n')^0
+  local key =
+    white_space *
+    capture(Range('az', 'AZ')^1) *
+    white_space
+
+  local name =
+    white_space *
+    capture(Range('az', 'AZ', '09', '++', '--')^1) *
+    white_space
+
+  local quote =
+    white_space *
+    Pattern('{') *
+    capture((1 - Pattern('}'))^0) * Pattern('}') *
+    white_space
+
+  local keyval = capture_group(
+    key *
+    Pattern('=') *
+    (quote + name) *
+    Pattern(',')^-1
+  )
+
+  local kvlist = capture_fold(capture_table('') * keyval^0, rawset)
+  return kvlist:match(input)
+end
+
 --- `nodex` is a abbreviation for __node eXtended__.
 local nodex = {}
 
@@ -1183,5 +1272,6 @@ export.reset = registry.unset_global_options
 export.get_defaults = registry.get_defaults
 export.get_value = registry.get_value
 export.marker = registry.write_marker
+export.key_value_parser = key_value_parser
 
 return export
