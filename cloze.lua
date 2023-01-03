6e729c50c32919254d102c56d7ec82876d724513
Josef Friedrich <josef@friedrich.rocks>
2023-01-03 22:25:38 +0100

    Update license header

--- cloze.lua
+++ cloze.lua
@@ -1,6 +1,21 @@
----Cloze uses [LDoc](https://github.com/stevedonovan/ldoc) for the
----source code documentation. The supported tags are described on in
----the [wiki](https://github.com/stevedonovan/LDoc/wiki).
+-- cloze.lua
+-- Copyright 2015-2023 Josef Friedrich
+--
+-- This work may be distributed and/or modified under the
+-- conditions of the LaTeX Project Public License, either version 1.3c
+-- of this license or (at your option) any later version.
+-- The latest version of this license is in
+--   http://www.latex-project.org/lppl.txt
+-- and version 1.3c or later is part of all distributions of LaTeX
+-- version 2008/05/04 or later.
+--
+-- This work has the LPPL maintenance status `maintained'.
+--
+-- The Current Maintainer of this work is Josef Friedrich.
+--
+-- This work consists of the files cloze.lua, cloze.tex,
+-- and cloze.sty.
+
 ---
 ---<h3>Naming conventions</h3>
 ---
